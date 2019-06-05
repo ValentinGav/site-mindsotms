dc58c5099568c34115aed65dc59e660779045683
ValentinGav <51400412+ValentinGav@users.noreply.github.com>
2019-06-05 11:02:06 +0200

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<? php
-		include_once ("pageprincipale.html");
-		include_once ("pagecode.html");
-		include_once ("pagephotos.html");
-		include_once ("téléchargement.png");
-		include_once ("menu.css");
-?>
