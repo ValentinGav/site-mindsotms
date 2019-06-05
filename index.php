416aae1f0f3a496aced78b2660ac96ba47fc1755
ValentinGav <51400412+ValentinGav@users.noreply.github.com>
2019-06-05 10:50:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 		include_once ("pageprincipale.html");
 		include_once ("pagecode.html");
 		include_once ("pagephotos.html");
-		include_once ("téléchargement.PNG");
+		include_once ("téléchargement.png");
 		include_once ("menu.css");
 ?>
